c919eef022f64257111040e45324ad8e607aceae
Shadesh7 <pascal.jordans@sundsits.de>
2018-04-23 15:23:15 +0200

    Beta 2

---
+++ src/Controllers/ContentController.php
@@ -0,0 +1,13 @@
+<?php
+namespace HelloWorld\Controllers;
+ 
+use Plenty\Plugin\Controller;
+use Plenty\Plugin\Templates\Twig;
+ 
+class ContentController extends Controller
+{
+    public function sayHello(Twig $twig):string
+    {
+         return $twig->render('HelloWorld::content.hello');
+    }
+}
---
+++ src/Providers/HelloWorldServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace HelloWorld\Providers;
+ 
+use Plenty\Plugin\ServiceProvider;
+ 
+class HelloWorldServiceProvider extends ServiceProvider
+{
+ 
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        $this->getApplication()->register(HelloWorldRouteServiceProvider::class);
+    }
+}
